dabb80d45ff242e08f411df4c6b0e25b2bc87182
HarryStevens <harryjosephstevens@gmail.com>
2026-07-06 20:50:12 -0400

    Improve interpose subdivision performance

--- src/utils/interpose.js
+++ src/utils/interpose.js
@@ -1,28 +1,42 @@
 import { angle, midpoint } from "./geometry.js";
 
+class Node {
+  constructor(point, next = null){
+    this.point = point;
+    this.next = next;
+  }
+}
+
 // Given a start point, an end point, and a prediciton function,
 // returns a smooth line.
 export function interpose(xmin, xmax, predict){
   const l = Math.log(xmax - xmin) * Math.LOG10E + 1 | 0;
   const precision = 1 * Math.pow(10, -l / 2 - 1), maxIter = 1e4;
-  let points = [px(xmin), px(xmax)], iter = 0;
+  const start = new Node(px(xmin), new Node(px(xmax)));
+  let iter = 0;
 
-  while (find(points) && iter < maxIter);
+  while (find() && iter < maxIter);
 
+  const points = [];
+  let current = start;
+  while (current){
+    points.push(current.point);
+    current = current.next;
+  }
   return points;
   
   function px(x){
     return [x, predict(x)];
   }
 
-  function find(points){
+  function find(){
     iter++;
-    const n = points.length;
     let found = false;
+    let current = start;
     
-    for (let i = 0; i < n - 1; i++){
-      const p0 = points[i],
-          p1 = points[i + 1],
+    while (current && current.next){
+      const p0 = current.point,
+          p1 = current.next.point,
           m = midpoint([p0, p1]),
           mp = px(m[0]),
           a0 = angle([p0, m]),
@@ -30,9 +44,10 @@ export function interpose(xmin, xmax, predict){
           a = Math.abs(a0 - a1);
       
       if (a > precision){
-        points.splice(i + 1, 0, mp);
+        current.next = new Node(mp, current.next);
         found = true;
       }
+      current = current.next;
     }
     
     return found;
